edd74f4fbf17000386a291d516074e0655048ec7
alyson-singleton <55365254+alyson-singleton@users.noreply.github.com>
2025-03-03 21:17:59 -0800

    rename files, standardize each year's columns, and store as base maps

--- 00_DNIT_2001_2012_clean.R
+++ 00_DNIT_2001_2012_clean.R
@@ -6,65 +6,87 @@ library(tidyverse)
 # load and standardize format 2001-2012
 #######################################
 
+#Brazilian Amazon boundaries
+brazil_amazon <- st_read("~/Desktop/doctorate/ch3 amazon network/data/Limites_Amazonia_Legal_2022_shp/Limites_Amazonia_Legal_2022.shp")
+brazil_amazon$geometry <- st_transform(brazil_amazon$geometry, 4326)
+
 #2001
 PNV_2001 <- read_excel("~/Desktop/doctorate/ch3 amazon network/data/DNIT_historical/PNV2001.xlsx")
 PNV_2001 <- PNV_2001[!is.na(PNV_2001$BR),]
 PNV_2001$vl_codigo <- PNV_2001$CODIGO
-PNV_2001$SUPERFICIE_ESTADUAL <- NA
-PNV_2001 <- PNV_2001[,c(1:11,14,12:13)]
+PNV_2001$name <- substr(PNV_2001$vl_codigo,1,6)
+PNV_2001$number <- substr(PNV_2001$vl_codigo,7,10)
+PNV_2001$SUP_ESTADUAL <- NA
+PNV_2001 <- PNV_2001[,c(1:7,9,8,10:11,16,12:15)]
 
 #2002
 PNV_2002 <- read_excel("~/Desktop/doctorate/ch3 amazon network/data/DNIT_historical/PNV2002.xlsx")
 PNV_2002 <- PNV_2002[!is.na(PNV_2002$BR),]
 PNV_2002$vl_codigo <- PNV_2002$CODIGO
-PNV_2002$SUPERFICIE_ESTADUAL <- NA
-PNV_2002 <- PNV_2002[,c(1:11,14,12:13)]
+PNV_2002$name <- substr(PNV_2002$vl_codigo,1,6)
+PNV_2002$number <- substr(PNV_2002$vl_codigo,7,10)
+PNV_2002$SUP_ESTADUAL <- NA
+PNV_2002 <- PNV_2002[,c(1:11,16,12:15)]
 
 #2003
 PNV_2003 <- read_excel("~/Desktop/doctorate/ch3 amazon network/data/DNIT_historical/PNV2003.xlsx")
 PNV_2003 <- PNV_2003[!is.na(PNV_2003$BR),]
 PNV_2003$vl_codigo <- PNV_2003$CODIGO
-PNV_2003$SUPERFICIE_ESTADUAL <- NA
-PNV_2003 <- PNV_2003[,c(1:11,14,12:13)]
+PNV_2003$name <- substr(PNV_2003$vl_codigo,1,6)
+PNV_2003$number <- substr(PNV_2003$vl_codigo,7,10)
+PNV_2003$SUP_ESTADUAL <- NA
+PNV_2003 <- PNV_2003[,c(1:11,16,12:15)]
 
 #2004
 PNV_2004 <- read_excel("~/Desktop/doctorate/ch3 amazon network/data/DNIT_historical/PNV2004.xlsx")
 PNV_2004 <- PNV_2004[!is.na(PNV_2004$BR),]
 PNV_2004$vl_codigo <- PNV_2004$CODIGO
-PNV_2004$SUPERFICIE_ESTADUAL <- NA
-PNV_2004 <- PNV_2004[,c(1:11,14,12:13)]
+PNV_2004$name <- substr(PNV_2004$vl_codigo,1,6)
+PNV_2004$number <- substr(PNV_2004$vl_codigo,7,10)
+PNV_2004$SUP_ESTADUAL <- NA
+PNV_2004 <- PNV_2004[,c(1:11,16,12:15)]
 
 #2005
 PNV_2005 <- read_excel("~/Desktop/doctorate/ch3 amazon network/data/DNIT_historical/PNV2005.xlsx")
 PNV_2005 <- PNV_2005[!is.na(PNV_2005$BR),]
 PNV_2005$vl_codigo <- PNV_2005$CODIGO
-PNV_2005$SUPERFICIE_ESTADUAL <- NA
-PNV_2005 <- PNV_2005[,c(1:11,14,12:13)]
+PNV_2005$name <- substr(PNV_2005$vl_codigo,1,6)
+PNV_2005$number <- substr(PNV_2005$vl_codigo,7,10)
+PNV_2005$SUP_ESTADUAL <- NA
+PNV_2005 <- PNV_2005[,c(1:11,16,12:15)]
 
 #2006
 PNV_2006 <- read_excel("~/Desktop/doctorate/ch3 amazon network/data/DNIT_historical/PNV2006.xlsx")
 PNV_2006 <- PNV_2006[!is.na(PNV_2006$BR),]
 PNV_2006$vl_codigo <- PNV_2006$CODIGO
-PNV_2006$SUPERFICIE_ESTADUAL <- NA
-PNV_2006 <- PNV_2006[,c(1:11,14,12:13)]
+PNV_2006$name <- substr(PNV_2006$vl_codigo,1,6)
+PNV_2006$number <- substr(PNV_2006$vl_codigo,7,10)
+PNV_2006$SUP_ESTADUAL <- NA
+PNV_2006 <- PNV_2006[,c(1:11,16,12:15)]
 
 #2007
 PNV_2007 <- read_excel("~/Desktop/doctorate/ch3 amazon network/data/DNIT_historical/PNV2007.xlsx")
 PNV_2007 <- PNV_2007[!is.na(PNV_2007$BR),]
 PNV_2007$vl_codigo <- PNV_2007$CODIGO
-colnames(PNV_2007)[12] <- "SUPERFICIE_ESTADUAL"
+PNV_2007$name <- substr(PNV_2007$vl_codigo,1,6)
+PNV_2007$number <- substr(PNV_2007$vl_codigo,7,10)
+colnames(PNV_2007)[12] <- "SUP_ESTADUAL" #check adding state PAV roads here / before
 
 #2008
 PNV_2008 <- read_excel("~/Desktop/doctorate/ch3 amazon network/data/DNIT_historical/PNV2008.xlsx")
 PNV_2008 <- PNV_2008[!is.na(PNV_2008$BR),]
 PNV_2008$vl_codigo <- PNV_2008$CODIGO
-colnames(PNV_2008)[12] <- "SUPERFICIE_ESTADUAL"
+PNV_2008$name <- substr(PNV_2008$vl_codigo,1,6)
+PNV_2008$number <- substr(PNV_2008$vl_codigo,7,10)
+colnames(PNV_2008)[12] <- "SUP_ESTADUAL" #check adding state PAV roads here / before
 
 #2009
 PNV_2009 <- read_excel("~/Desktop/doctorate/ch3 amazon network/data/DNIT_historical/PNV2009.xlsx")
 PNV_2009 <- PNV_2009[!is.na(PNV_2009$BR),]
 PNV_2009$vl_codigo <- PNV_2009$CODIGO
-colnames(PNV_2009)[12] <- "SUPERFICIE_ESTADUAL"
+PNV_2009$name <- substr(PNV_2009$vl_codigo,1,6)
+PNV_2009$number <- substr(PNV_2009$vl_codigo,7,10)
+colnames(PNV_2009)[12] <- "SUP_ESTADUAL"
 
 #2010
 PNV_2010 <- read_excel("~/Desktop/doctorate/ch3 amazon network/data/DNIT_historical/PNV2010.xlsx")
@@ -73,7 +95,9 @@ PNV_2010 <- PNV_2010[-c(1,2),]
 PNV_2010 <- PNV_2010[!is.na(PNV_2010$BR),]
 PNV_2010$VERSAO <- 2010
 PNV_2010$vl_codigo <- PNV_2010$Código
-PNV_2010 <- PNV_2010[,c(1:10,13:14,17:18)]
+PNV_2010$name <- substr(PNV_2010$vl_codigo,1,6)
+PNV_2010$number <- substr(PNV_2010$vl_codigo,7,10)
+PNV_2010 <- PNV_2010[,c(1:10,13:14,17:20)]
 colnames(PNV_2010) <- colnames(PNV_2009)
 
 #2011
@@ -83,7 +107,9 @@ PNV_2011 <- PNV_2011[-c(1,2),]
 PNV_2011 <- PNV_2011[!is.na(PNV_2011$BR),]
 PNV_2011$VERSAO <- 2011
 PNV_2011$vl_codigo <- PNV_2011$Código
-PNV_2011 <- PNV_2011[,c(1,2,6:13,16,17,20,21)]
+PNV_2011$name <- substr(PNV_2011$vl_codigo,1,6)
+PNV_2011$number <- substr(PNV_2011$vl_codigo,7,10)
+PNV_2011 <- PNV_2011[,c(1:2,6:13,16:17,20:23)]
 colnames(PNV_2011) <- colnames(PNV_2009)
 
 #2012
@@ -93,7 +119,9 @@ PNV_2012 <- PNV_2012[-c(1,2),]
 PNV_2012 <- PNV_2012[!is.na(PNV_2012$BR),]
 PNV_2012$VERSAO <- 2012
 PNV_2012$vl_codigo <- PNV_2012$Código
-PNV_2012 <- PNV_2012[,c(1,2,6:13,16,17,21,22)]
+PNV_2012$name <- substr(PNV_2012$vl_codigo,1,6)
+PNV_2012$number <- substr(PNV_2012$vl_codigo,7,10)
+PNV_2012 <- PNV_2012[,c(1:2,6:13,16:17,21:24)]
 colnames(PNV_2012) <- colnames(PNV_2009)
 
 #######################################
@@ -105,45 +133,86 @@ DNIT_2024 <- st_read("~/Desktop/doctorate/ch3 amazon network/data/DNIT/202410A/S
 DNIT_2024$geometry <- st_transform(st_zm(DNIT_2024$geometry), 4326)
 DNIT_2024_bool <- st_covers(brazil_amazon,DNIT_2024$geometry, sparse = FALSE)
 DNIT_2024_amazon <- DNIT_2024[DNIT_2024_bool[1,],]
-DNIT_2024_amazon_paved <- DNIT_2024_amazon[which(DNIT_2024_amazon$ds_superfi %in% c('PAV') | DNIT_2024_amazon$sup_est_co %in% c('PAV', 'DUP')),]
+DNIT_2024_amazon_paved <- DNIT_2024_amazon[which(DNIT_2024_amazon$sg_legenda %in% c('PAV', 'DUP', 'EOD') | DNIT_2024_amazon$sup_est_co %in% c('PAV', 'DUP', 'EOD')),]
 DNIT_2024_reduced <- DNIT_2024_amazon_paved[,c(7,29)]
 
 #loop to add 2024 shapefiles to each year (2001-2012)
-df_names <- paste("PNV_",2001:2012,sep="")
-for (i in df_names){
-  print(i)
-  #restart here
-  join_left_2001 <- left_join(PNV_2001,DNIT_2024_reduced, by=c("vl_codigo"))
-  joined_sf <- st_as_sf(join_left_2001)
-  joined_sf_bool <- st_covers(brazil_amazon,joined_sf$geometry, sparse = FALSE)
-  joined_sf_amazon <- joined_sf[joined_sf_bool[1,],]
-  joined_sf_amazon_paved <- joined_sf_amazon[which(joined_sf_amazon$SUPERFICIE  %in% c("PAV","DUP")),]
-  DNIT_2001_amazon_paved <- joined_sf_amazon_paved
+years <- 2001:2012
+#year <- 2002
+for (year in years){
+  print(year)
+  
+  old_df_name <- paste0("PNV_", year, sep="")
+  df <- get(old_df_name)
+  
+  joined_left_sf <- left_join(df,DNIT_2024_reduced, by=c("vl_codigo")) %>% st_as_sf()
+  joined_sf_bool <- st_covers(brazil_amazon,joined_left_sf$geometry, sparse = FALSE)
+  joined_sf_amazon <- joined_left_sf[joined_sf_bool[1,],]
+  #NOTE: write now ive written it like we don't care about duplicated v non duplicated, just PAVED
+  joined_sf_amazon_paved <- joined_sf_amazon[which(joined_sf_amazon$SUPERFICIE  %in% c('PAV', 'DUP', 'EOD') | joined_sf_amazon$SUP_ESTADUAL %in% c('PAV', 'DUP', 'EOD')),]
+  
+  new_df_name <- paste0("DNIT_", year, "_amazon_paved", sep="")
+  assign(new_df_name, joined_sf_amazon_paved)
 }
 
-join_left_2001 <- left_join(PNV_2001,DNIT_2024_reduced, by=c("vl_codigo"))
-joined_sf <- st_as_sf(join_left_2001)
-joined_sf_bool <- st_covers(brazil_amazon,joined_sf$geometry, sparse = FALSE)
-joined_sf_amazon <- joined_sf[joined_sf_bool[1,],]
-joined_sf_amazon_paved <- joined_sf_amazon[which(joined_sf_amazon$SUPERFICIE  %in% c("PAV","DUP")),]
-DNIT_2001_amazon_paved <- joined_sf_amazon_paved
+#######################################
+# fill in spottiness
+#######################################
 
-
-
-############### SCRATCH ############### 
-join_full_2001 <- full_join(PNV_2001,DNIT_2024_reduced, by=c("vl_codigo"))
+join_right_2001 <- right_join(PNV_2001,DNIT_2024_reduced, by=c("vl_codigo")) #is DNIT_2024_reduced just amazon roads here? can PNV_2001 be DNIT_2001_amazon_paved?
 join_inner_2001 <- inner_join(PNV_2001,DNIT_2024_reduced, by=c("vl_codigo"))
-join_left_2001 <- left_join(PNV_2001,DNIT_2024_reduced, by=c("vl_codigo"))
-join_right_2001 <- right_join(PNV_2001,DNIT_2024_reduced, by=c("vl_codigo"))
 
-joined_sf <- st_as_sf(join_left_2001)
-joined_sf_bool <- st_covers(brazil_amazon,joined_sf$geometry, sparse = FALSE)
-joined_sf_amazon <- joined_sf[joined_sf_bool[1,],]
-joined_sf_amazon_paved <- joined_sf_amazon[which(joined_sf_amazon$SUPERFICIE  %in% c("PAV","DUP")),]
-DNIT_2001_amazon_paved <- joined_sf_amazon_paved
+#2024 no match
+codes_w_match <- unique(join_right_2001$vl_codigo)[unique(join_right_2001$vl_codigo) %in% unique(join_inner_2001$vl_codigo)]
+join_right_2001_match_col <- join_right_2001 %>%
+  mutate(match = join_right_2001$vl_codigo %in% codes_w_match)
+join_right_2001_match_col$name <- substr(join_right_2001_match_col$vl_codigo,1,6)
+join_right_2001_match_col$number <- substr(join_right_2001_match_col$vl_codigo,7,10)
+join_right_2001_match_col <- join_right_2001_match_col[,c(1:3,18,4:17)]
 
-mapview(DNIT_2001_amazon_paved)
-mapview(DNIT_2024_amazon_paved)
+#loop to fill in missing pieces that don't reflect actual paving status (i.e., known to paved in 2001)
+our_list <- c("364BRO","174BRR","364BMT","163BMT","070BMT","163BMT","153BTO", "010BMA", "230BTO") #all: unique(DNIT_2001_amazon_paved$name)
+years <- 2001:2012
+
+for (year in years){
+  print(year)
+  old_df_name <- paste0("DNIT_", year, "_amazon_paved", sep="")
+  df <- get(old_df_name)
+  df$match <- TRUE
+  df <- df[,c(1:3,18,4:17)]
+  for (i in 1:length(our_list)) {
+    name_i <- our_list[i]
+    all_unmatched_2024_name_i <- join_right_2001_match_col[which(join_right_2001_match_col$name == name_i & join_right_2001_match_col$match == FALSE),] %>% st_as_sf()
+    df <- rbind(df,all_unmatched_2024_name_i)
+  }
+  new_df_name <- paste0("DNIT_", year, "_amazon_paved_filled", sep="")
+  assign(new_df_name, df)
+}
+
+#check
+mapview(DNIT_2001_amazon_paved_filled)
+mapview(DNIT_2002_amazon_paved_filled)
+mapview(DNIT_2003_amazon_paved_filled)
+mapview(DNIT_2004_amazon_paved_filled)
+mapview(DNIT_2005_amazon_paved_filled)
+mapview(DNIT_2006_amazon_paved_filled)
+mapview(DNIT_2007_amazon_paved_filled)
+mapview(DNIT_2008_amazon_paved_filled)
+mapview(DNIT_2009_amazon_paved_filled)
+mapview(DNIT_2010_amazon_paved_filled)
+mapview(DNIT_2011_amazon_paved_filled)
+mapview(DNIT_2012_amazon_paved_filled)
+
+#store
+for (year in years){
+  df_name <- paste0("DNIT_", year, "_amazon_paved_filled", sep="")
+  df <- get(df_name)
+  st_write(df, paste0("~/Desktop/doctorate/ch3 amazon network/data/DNIT_processed/DNIT_yearly_base_maps/", "DNIT_", year, "_base_map.shp", sep=""))
+}
+
+#######################################
+############### SCRATCH ############### 
+#######################################
 
 # main concern 1: some 2001 codes dont match 2024 (~700)
 # main concern 2: lots of spottiness that doesn't seem to reflect actual paving status
@@ -214,35 +283,4 @@ mapview(DNIT_2001_amazon_paved)
 mapview(DNIT_2013_amazon_paved)
 mapview(DNIT_2024_amazon_paved)
 
-#########################################################
-#now let's look into changes between 2001 and 2006 (halfway through)
-#########################################################
 
-PNV_2006 <- read_excel("~/Desktop/doctorate/ch3 amazon network/data/DNIT_historical/PNV2006.xlsx")
-PNV_2006 <- PNV_2006[!is.na(PNV_2006$BR),]
-PNV_2006$vl_codigo <- PNV_2006$CODIGO
-join_left_2006 <- left_join(PNV_2006,DNIT_2024_reduced, by=c("vl_codigo"))
-joined_sf <- st_as_sf(join_left_2006)
-joined_sf_bool <- st_covers(brazil_amazon,joined_sf$geometry, sparse = FALSE)
-joined_sf_amazon <- joined_sf[joined_sf_bool[1,],]
-joined_sf_amazon_paved <- joined_sf_amazon[which(joined_sf_amazon$SUPERFICIE  %in% c("PAV","DUP")),]
-DNIT_2006_amazon_paved <- joined_sf_amazon_paved
-mapview(DNIT_2006_amazon_paved)
-
-#########################################################
-#2012
-#########################################################
-
-SNV_2012 <- read_excel("~/Desktop/doctorate/ch3 amazon network/data/DNIT_historical/SNV_2012.xlsx")
-colnames(SNV_2012) <- SNV_2012[2,]
-SNV_2012 <- SNV_2012[-c(1,2),]
-SNV_2012 <- SNV_2012[!is.na(SNV_2012$BR),]
-SNV_2012$vl_codigo <- SNV_2012$Código
-colnames(SNV_2012)[13:15] <- c("FedCoin1","FedCoin2","FedCoin3")
-join_left_2012 <- left_join(SNV_2012,DNIT_2024_reduced, by=c("vl_codigo"))
-joined_sf <- st_as_sf(join_left_2012)
-joined_sf_bool <- st_covers(brazil_amazon,joined_sf$geometry, sparse = FALSE)
-joined_sf_amazon <- joined_sf[joined_sf_bool[1,],]
-joined_sf_amazon_paved <- joined_sf_amazon[which(joined_sf_amazon$Superficie  %in% c("PAV","DUP")),]
-DNIT_2012_amazon_paved <- joined_sf_amazon_paved
-mapview(DNIT_2012_amazon_paved)
---
+++ 01_DNIT_2013_2024_clean.R
@@ -0,0 +1,166 @@
+library(readxl)
+library(sf)
+library(tidyverse)
+
+#######################################
+# load and standardize format 2013-2024
+#######################################
+
+#Brazilian Amazon boundaries
+brazil_amazon <- st_read("~/Desktop/doctorate/ch3 amazon network/data/Limites_Amazonia_Legal_2022_shp/Limites_Amazonia_Legal_2022.shp")
+brazil_amazon$geometry <- st_transform(brazil_amazon$geometry, 4326)
+
+set_colnames <- c("BR", "UF", "CODIGO", "LOCAL_INI", "LOCAL_FIM", "KM_INI", "KM_FIM", "EXTENSAO", "SUPERFICIE", 
+                  "TR_COINCID", "EST_COINCID","SUP_ESTADUAL", "VERSAO", "vl_codigo", "name", "number", "geometry")
+
+#2013
+DNIT_2013 <- st_read("~/Desktop/doctorate/ch3 amazon network/data/DNIT/201301A/ST_DNIT_RODOVIAS_SNV2013_COMPLETO.shp")
+DNIT_2013$VERSAO <- 2013
+DNIT_2013$vl_codigo <- DNIT_2013$Código
+DNIT_2013$name <- substr(DNIT_2013$vl_codigo,1,6)
+DNIT_2013$number <- substr(DNIT_2013$vl_codigo,7,10)
+DNIT_2013 <- DNIT_2013[,c(2:3,7:14,17:18,24:27,23)]
+colnames(DNIT_2013) <- set_colnames
+  
+#2015
+DNIT_2015 <- st_read("~/Desktop/doctorate/ch3 amazon network/data/DNIT/201503A/snv_201503a.shp")
+DNIT_2015$VERSAO <- 2015
+DNIT_2015$vl_codigo <- DNIT_2015$codigo
+DNIT_2015$name <- substr(DNIT_2015$vl_codigo,1,6)
+DNIT_2015$number <- substr(DNIT_2015$vl_codigo,7,10)
+DNIT_2015 <- DNIT_2015[,c(3:12,15:16,21:24,20)]
+colnames(DNIT_2015) <- set_colnames
+
+#2016
+DNIT_2016 <- st_read("~/Desktop/doctorate/ch3 amazon network/data/DNIT/201606A/Rodovias_federais_snv.shp") #other 2016 ops didnt have "ds_sup_fed"
+DNIT_2016$VERSAO <- 2016
+colnames(DNIT_2016)[8] <- "codigo"
+DNIT_2016$vl_codigo <- DNIT_2016$codigo
+DNIT_2016$name <- substr(DNIT_2016$vl_codigo,1,6)
+DNIT_2016$number <- substr(DNIT_2016$vl_codigo,7,10)
+DNIT_2016 <- DNIT_2016[,c(2:3,8:15,18:19,24:27,23)]
+colnames(DNIT_2016) <- set_colnames
+
+#2017
+DNIT_2017 <- st_read("~/Desktop/doctorate/ch3 amazon network/data/DNIT/201710B/SNV_201710B.shp")
+DNIT_2017$VERSAO <- 2017
+colnames(DNIT_2017)[7] <- "codigo"
+DNIT_2017$vl_codigo <- DNIT_2017$codigo
+DNIT_2017$name <- substr(DNIT_2017$vl_codigo,1,6)
+DNIT_2017$number <- substr(DNIT_2017$vl_codigo,7,10)
+DNIT_2017 <- DNIT_2017[,c(2:3,7:12,23,15,18:19,29:32,28)]
+colnames(DNIT_2017) <- set_colnames
+
+#2018
+DNIT_2018 <- st_read("~/Desktop/doctorate/ch3 amazon network/data/DNIT/201811A/snv_201811A.shp")
+DNIT_2018$VERSAO <- 2018
+colnames(DNIT_2018)[7] <- "codigo"
+DNIT_2018$vl_codigo <- DNIT_2018$codigo
+DNIT_2018$name <- substr(DNIT_2018$vl_codigo,1,6)
+DNIT_2018$number <- substr(DNIT_2018$vl_codigo,7,10)
+DNIT_2018 <- DNIT_2018[,c(2:3,7:12,24,16,19:20,30:33,29)]
+colnames(DNIT_2018) <- set_colnames
+
+#2019
+DNIT_2019 <- st_read("~/Desktop/doctorate/ch3 amazon network/data/DNIT/201910A/SNV_201910A.shp")
+DNIT_2019$VERSAO <- 2019
+colnames(DNIT_2019)[7] <- "codigo"
+DNIT_2019$vl_codigo <- DNIT_2019$codigo
+DNIT_2019$name <- substr(DNIT_2019$vl_codigo,1,6)
+DNIT_2019$number <- substr(DNIT_2019$vl_codigo,7,10)
+DNIT_2019 <- DNIT_2019[,c(2:3,7:12,24,16,19:20,30:33,29)]
+colnames(DNIT_2019) <- set_colnames
+
+#2020
+DNIT_2020 <- st_read("~/Desktop/doctorate/ch3 amazon network/data/DNIT/202010A/SNV_202010A.shp")
+DNIT_2020$VERSAO <- 2020
+colnames(DNIT_2020)[7] <- "codigo"
+DNIT_2020$vl_codigo <- DNIT_2020$codigo
+DNIT_2020$name <- substr(DNIT_2020$vl_codigo,1,6)
+DNIT_2020$number <- substr(DNIT_2020$vl_codigo,7,10)
+DNIT_2020 <- DNIT_2020[,c(2:3,7:12,24,16,19:20,30:33,29)]
+colnames(DNIT_2020) <- set_colnames
+
+#2021
+DNIT_2021 <- st_read("~/Desktop/doctorate/ch3 amazon network/data/DNIT/202110A/SNV_202110A.shp")
+DNIT_2021$VERSAO <- 2021
+colnames(DNIT_2021)[7] <- "codigo"
+DNIT_2021$vl_codigo <- DNIT_2021$codigo
+DNIT_2021$name <- substr(DNIT_2021$vl_codigo,1,6)
+DNIT_2021$number <- substr(DNIT_2021$vl_codigo,7,10)
+DNIT_2021 <- DNIT_2021[,c(2:3,7:12,24,16,19:20,30:33,29)]
+colnames(DNIT_2021) <- set_colnames
+
+#2022
+DNIT_2022 <- st_read("~/Desktop/doctorate/ch3 amazon network/data/DNIT/202210C/SNV_202210C.shp")
+DNIT_2022$VERSAO <- 2022
+colnames(DNIT_2022)[7] <- "codigo"
+DNIT_2022$vl_codigo <- DNIT_2022$codigo
+DNIT_2022$name <- substr(DNIT_2022$vl_codigo,1,6)
+DNIT_2022$number <- substr(DNIT_2022$vl_codigo,7,10)
+DNIT_2022 <- DNIT_2022[,c(2:3,7:12,24,16,19:20,30:33,29)]
+colnames(DNIT_2022) <- set_colnames
+
+#2023
+DNIT_2023 <- st_read("~/Desktop/doctorate/ch3 amazon network/data/DNIT/202310A/SNV_202310A.shp")
+DNIT_2023$VERSAO <- 2023
+colnames(DNIT_2023)[7] <- "codigo"
+DNIT_2023$vl_codigo <- DNIT_2023$codigo
+DNIT_2023$name <- substr(DNIT_2023$vl_codigo,1,6)
+DNIT_2023$number <- substr(DNIT_2023$vl_codigo,7,10)
+DNIT_2023 <- DNIT_2023[,c(2:3,7:12,24,16,19:20,30:33,29)]
+colnames(DNIT_2023) <- set_colnames
+
+#2024
+DNIT_2024 <- st_read("~/Desktop/doctorate/ch3 amazon network/data/DNIT/202410A/SNV_202410A.shp")
+DNIT_2024$VERSAO <- 2024
+colnames(DNIT_2024)[7] <- "codigo"
+DNIT_2024$vl_codigo <- DNIT_2024$codigo
+DNIT_2024$name <- substr(DNIT_2024$vl_codigo,1,6)
+DNIT_2024$number <- substr(DNIT_2024$vl_codigo,7,10)
+DNIT_2024 <- DNIT_2024[,c(2:3,7:12,24,16,19:20,30:33,29)]
+colnames(DNIT_2024) <- set_colnames
+
+#######################################
+# reduce to paved and in the amazon
+#######################################
+
+years <- c(2013,2015:2024)
+for (year in years){
+  print(year)
+  
+  old_df_name <- paste0("DNIT_", year, sep="")
+  df <- get(old_df_name)
+  
+  df$geometry <- st_transform(st_zm(df$geometry), 4326)
+  df_bool <- st_covers(brazil_amazon,df$geometry, sparse = FALSE)
+  df_amazon <- df[df_bool[1,],]
+  #NOTE: write now ive written it like we don't care about duplicated v non duplicated, just PAVED
+  df_amazon_paved <- df_amazon[which(df_amazon$SUPERFICIE  %in% c('PAV', 'DUP', 'EOD') | df_amazon$SUP_ESTADUAL %in% c('PAV', 'DUP', 'EOD')),]
+  
+  new_df_name <- paste0("DNIT_", year, "_amazon_paved", sep="")
+  assign(new_df_name, df_amazon_paved)
+}
+
+#check
+mapview(DNIT_2001_amazon_paved_filled)
+mapview(DNIT_2012_amazon_paved_filled)#double check jumps from 2012-2013
+mapview(DNIT_2013_amazon_paved)
+mapview(DNIT_2015_amazon_paved)
+mapview(DNIT_2016_amazon_paved)
+mapview(DNIT_2017_amazon_paved)
+mapview(DNIT_2018_amazon_paved)
+mapview(DNIT_2019_amazon_paved)
+mapview(DNIT_2020_amazon_paved)
+mapview(DNIT_2021_amazon_paved)
+mapview(DNIT_2022_amazon_paved)
+mapview(DNIT_2023_amazon_paved)
+mapview(DNIT_2024_amazon_paved)
+
+#store
+years <- c(2013,2015:2024)
+for (year in years){
+  df_name <- paste0("DNIT_", year, "_amazon_paved", sep="")
+  df <- get(df_name)
+  st_write(df, paste0("~/Desktop/doctorate/ch3 amazon network/data/DNIT_processed/DNIT_yearly_base_maps/", "DNIT_", year, "_base_map.shp", sep=""))
+}
